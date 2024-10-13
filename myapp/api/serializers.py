affb59b3e4350fb059e717a05f91ed21086c39ab
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-10-13 13:03:45 +0400

    add new properties

--- myapp/api/serializers.py
+++ myapp/api/serializers.py
@@ -4,6 +4,7 @@ from ..models import Note  # Import the Note model
 from ..models import CustomUser
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from django.core.validators import FileExtensionValidator
 
 
 # Create a serializer class
@@ -12,6 +13,14 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().update(instance, validated_data)
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -37,6 +46,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         write_only=True, required=True, validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
+    cover_photo = serializers.ImageField(
+        validators=[
+            FileExtensionValidator(allowed_extensions=["jpg", "jpeg", "png"]),
+        ],
+        required=False,
+    )
 
     class Meta:
         model = CustomUser
@@ -70,3 +85,12 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = "__all__"
+
+    def to_internal_value(self, data):
+        cleaned_data = {}
+        for key, value in data.items():
+            if isinstance(value, bytes):
+                cleaned_data[key] = value.decode("utf-8", errors="replace")
+            else:
+                cleaned_data[key] = value
+        return super().to_internal_value(cleaned_data)
--- myapp/models.py
+++ myapp/models.py
@@ -10,37 +10,31 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f"{self.bio}"
 
-    groups = models.ManyToManyField(
-        "auth.Group",
-        related_name="customuser_set",  # Değiştirin
-        blank=True,
-        help_text="The groups this user belongs to.",
-        verbose_name="groups",
-    )
+    # groups = models.ManyToManyField(
+    #     "auth.Group",
+    #     related_name="customuser_set",  # Değiştirin
+    #     blank=True,
+    #     help_text="The groups this user belongs to.",
+    #     verbose_name="groups",
+    # )
 
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        related_name="customuser_set",  # Değiştirin
-        blank=True,
-        help_text="Specific permissions for this user.",
-        verbose_name="user permissions",
-    )
+    # user_permissions = models.ManyToManyField(
+    #     "auth.Permission",
+    #     related_name="customuser_set",  # Değiştirin
+    #     blank=True,
+    #     help_text="Specific permissions for this user.",
+    #     verbose_name="user permissions",
+    # )
 
 
 class Note(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.SET_NULL,null=True,blank=True,related_name="notes")
+    title=models.CharField(max_length=100,null=True,blank=True)
+    cover_image=models.ImageField(upload_to="images/",null=True,blank=True)
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    is_public = models.BooleanField(
-        default=False, null=True, blank=True
-    )  # Varsayılan değeri False
-    # user = models.ForeignKey(
-    #     CustomUser,
-    #     on_delete=models.CASCADE,
-    #     null=True,
-    #     blank=True,
-    #     related_name="notes",
-    # )
+    is_public = models.BooleanField(default=False, null=True, blank=True)
 
     # This is the string representation of the object
     def __str__(self):
